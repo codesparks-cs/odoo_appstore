4bf640d0a0d7e8c39efdc6a7c7d4a45ddbe51b97
Code Sparks <info.codesparks@gmail.com>
2024-11-16 00:16:08 +0530

    [MOD] Update discount module

--- cs_discount_price/__manifest__.py
+++ cs_discount_price/__manifest__.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Discount On Price",
-    "version": "16.0.0",
+    "version": "16.0.0.0",
+    "category": "Invoicing",
     "summary": "Calculates Discount percentage on change of Discount Price.",
     'sequence': 1,
     "discription": """
         The Discount Calculator Application is designed to effortlessly compute discount percentages based on changes in price, as well as determine the discounted price from a given discount percentage.
     """,
     "author": "Code Sparks",
+    "company": "Code Sparks",
+    "maintainer": "Code Sparks",
     "website": "",
-    "license": "LGPL-3",
-    "category": "Invoicing",
-    "depends": ["base", "account"],
+    "depends": ["base", "account", "sale_management"],
     "data": [
-        "views/account_move_inherit.xml"
+        "views/sale_account_inherit.xml"
     ],
     "images": ['static/description/banner.png'],
+    "license": "LGPL-3",
     "installable": True,
     "auto_install": False,
     "application": True,
--- cs_discount_price/models/__init__.py
+++ cs_discount_price/models/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-from . import account_move_line
+from . import sale_account_line
